c4bc426cb5b427d61faa88dcf784c058d74cf91c
Juan Cruz Ambrosini <juancruz.ambrosini@gmail.com>
2025-04-19 00:22:16 -0300

    Finalización TP4 parte 1 Lógica

--- ALGORITMOS Y ESTRUCTURA DE DATOS 1/TPs/Python files/T.P.N4/mylinkedlist.py
+++ ALGORITMOS Y ESTRUCTURA DE DATOS 1/TPs/Python files/T.P.N4/mylinkedlist.py
@@ -35,17 +35,6 @@ def search(list, element):
     
 """---------------------------------------------------------------------------------------------"""
 
-"""insert(L,element,position)
-Descripción: Inserta un elemento en una posición determinada de la
-lista que representa el TAD secuencia.
-Entrada: la lista sobre el cual se quiere realizar la inserción
-(Linkedlist) y el valor del elemento (element) a insertar y la
-posición (position) donde se quiere insertar.
-Salida: Si pudo insertar con éxito devuelve la posición donde se
-inserta el elemento. En caso contrario devuelve None. Devuelve None si
-la posición a insertar es mayor que el número de elementos en la
-lista."""
-
 def insert(list, element, index):
     if length(list) < index :
         return None
@@ -53,17 +42,36 @@ def insert(list, element, index):
         current_node = list.head
         new_node = Node()
         new_node.value = element
-        for i in range (0,index + 1):
-            if index == 0:
-                add(list, element)
-                return index
-            else:
-                if (i - 1) == index:
-                    new_node.nextNode = current_node.nextNode
-                    current_node.nextNode = new_node
-            current_node = current_node.nextNode
-        return index
+        if index == 0:
+            add(list, element)
+            return index
+        else: 
+            for i in range (1,length(list) + 1):
+                    if i == index:
+                        new_node.nextNode = current_node.nextNode
+                        current_node.nextNode = new_node
+                        break
+                    current_node = current_node.nextNode
+            return index
     
+"""---------------------------------------------------------------------------------------------"""
+
+def delete(list, element):
+    element_position = search(list, element)
+    current_node = list.head
+    if element_position == None:
+        return None
+    else:
+        if (current_node.value == element):
+            list.head = list.head.nextNode
+        else:
+            while (current_node.nextNode != None):
+                if current_node.nextNode.value == element :
+                    current_node.nextNode = current_node.nextNode.nextNode
+                    break
+                current_node = current_node.nextNode
+        return element_position
+
 """---------------------------------------------------------------------------------------------"""
 
 def mostrarLista (lista):
@@ -74,21 +82,61 @@ def mostrarLista (lista):
         
 """---------------------------------------------------------------------------------------------"""
 
+def update (list, element, index):
+    current_node = list.head
+    size = length(list) - 1
+    value = None
+    
+    if index > size or index < 0 :
+        return None
+    for i in range(0, size + 1):
+        if i == index:
+            current_node.value = element
+            return index
+        current_node = current_node.nextNode
+
+"""---------------------------------------------------------------------------------------------"""
+
+def access(list, index):
+    current_node = list.head
+    size = length(list) - 1
+    value = None
+    
+    if index > size or index < 0 :
+        return None
+    
+    for i in range(0, size + 1):
+        if i == index:
+            value = current_node.value
+            return value
+        current_node = current_node.nextNode
+
+"""---------------------------------------------------------------------------------------------"""
+
 def length(list):
     lenght = 0
     current_node = list.head
     while current_node != None: 
-        lenght = lenght + 1
         current_node = current_node.nextNode
+        lenght = lenght + 1
     return lenght
 
 """---------------------------------------------------------------------------------------------"""
 
 lista = LinkedList()
+add(lista, 1)
+add(lista, 2)
+add(lista, 3)
 add(lista, 4)
-add(lista, 6)
-insert(lista, 8, 1)
-print(insert(lista, 8, 1))
+insert(lista, 0, 3)
+delete(lista,1)
+print("-------------------------------------------------")
+print(access(lista, 4))
+print("-------------------------------------------------")
 print(search(lista, 6))
+print("-------------------------------------------------")
+mostrarLista(lista)
+print("-------------------------------------------------")
+update(lista, 20, 3)
 mostrarLista(lista)
 
